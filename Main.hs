b9e2413c4267bd65ed612ba119d94fc051f288be
Dave Compton <davecompton7@gmail.com>
2017-04-25 19:52:13 +0000

    cleaned up quite a bit

--- Main.hs
+++ Main.hs
@@ -1,66 +1,75 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE JavaScriptFFI, CPP #-}
+{-# LANGUAGE JavaScriptFFI #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 import Reflex.Dom
 import Data.Monoid ((<>))
 import Control.Monad.IO.Class ( liftIO )
-import GHCJS.DOM.HTMLCanvasElement 
-import GHCJS.DOM.JSFFI.Generated.CanvasRenderingContext2D
+import GHCJS.DOM.HTMLCanvasElement (getContext)
+import GHCJS.DOM.JSFFI.Generated.CanvasRenderingContext2D (putImageData, fillRect, setFillStyle)
 import GHCJS.DOM.Types (CanvasStyle(..), CanvasRenderingContext2D(..), toJSString, castToHTMLCanvasElement, ImageData(..))
 import Foreign.Ptr (Ptr)
 import GHCJS.Types (JSVal)
 import GHCJS.Marshal.Pure
 import GHCJS.Marshal (toJSVal)
-import Data.Map as M
-import Data.Text as T
-import Data.ByteString as BS
+import Data.Map (Map)
+import Data.Text as T (Text, pack)
+import Data.ByteString as BS (pack, useAsCStringLen)
 
 -- http://lpaste.net/154691
 -- https://www.snip2code.com/Snippet/1032978/Simple-Canvas-Example/
-
-canvasAttrs :: Float -> Float -> Map Text Text
-canvasAttrs w h = ("width" =: (T.pack $ show w)) <> ("height" =: (T.pack $ show h))
 
 foreign import javascript unsafe 
     -- Arguments
     --    width  : JSNumber
     --    height : JSNumber
     --    pixels : Ptr a -- Pointer to a ByteString in the format below
+    --    length : JSNumber -- number of pixels
     "(function(){                                     \
         var width  = $1;                              \
         var height = $2;                              \
-        console.log(width);                           \
-        console.log(height);                           \
-        console.log($3);                           \
-        console.log($3.u8[0]);                           \
         var length = $4;                                         \
         var buffer = $3.u8.slice(0, length);                                         \
-        console.log(length);                           \
-        var pixels = new Uint8ClampedArray(buffer,0,length);    \
+        var pixels = new Uint8ClampedArray(buffer);    \
         return new ImageData(pixels, width, height)   \
     })()" 
-    newImageData :: forall a . JSVal -> JSVal -> Ptr a -> JSVal -> IO JSVal
+    jsImageData :: forall a . JSVal -> JSVal -> Ptr a -> JSVal -> IO JSVal
+
+newImageData :: Int -> Int -> (Ptr a, Int) -> IO JSVal
+newImageData width height (ptr, len) = 
+    jsImageData (pToJSVal width) (pToJSVal height) ptr (pToJSVal len)
+
+canvasAttrs :: Int -> Int -> Map T.Text T.Text
+canvasAttrs w h =    ("width" =: (T.pack $ show w)) 
+                  <> ("height" =: (T.pack $ show h))
 
 main = mainWidget $ do
-    let width = 300.0
-    let height = 200.0
-    (canvasEl, _) <- elAttr' "canvas" (canvasAttrs width height) $ return ()
-    let canvasElement = castToHTMLCanvasElement(_element_raw canvasEl)
-    let elementContext =  getContext canvasElement ("2d" :: [Char])
-    c <- fmap CanvasRenderingContext2D $ liftIO elementContext
+    let canvasWidth = 300
+        canvasHeight = 200
 
-    let imageWidth :: Float= 4.0
-    let imageHeight :: Float= 3.0
-    let smallImage = BS.pack [0xff,0x00,0x00,0xff,  0xff,0x00,0x00,0xff,  0xff,0x00,0x00,0xff,
-                              0x00,0x00,0x00,0xff,  0x00,0xff,0x00,0xff,  0x00,0x00,0x00,0xff,
-                              0x00,0x00,0xff,0xff,  0x00,0x00,0xff,0xff,  0x00,0x00,0xff,0xff,
-                              0x00,0x00,0xff,0xff,  0x00,0x00,0x00,0xff,  0x00,0x00,0xff,0xff]
-    smallImageData <- liftIO $ BS.useAsCStringLen smallImage $ \ (ptr,len) -> newImageData (pToJSVal imageWidth) (pToJSVal imageHeight) ptr (pToJSVal len)
+        boxWidth = 50
+        boxHeight = 30
 
+        imageWidth = boxWidth
+        imageHeight = boxHeight * 3
 
-    liftIO $ setFillStyle c =<< (fmap (Just . CanvasStyle) $ toJSVal $ toJSString ("red"::[Char]))
-    fillRect c 0.0 0.0 width height
-    liftIO $ putImageData c (Just $ ImageData smallImageData) 100 150
+        reds = take (boxWidth*boxHeight*4) $ concat $ repeat [0xff,0x00,0x00,0xff]
+        blues = take (boxWidth*boxHeight*4) $ concat $ repeat [0x00,0xff,0x00,0xff]
+        greens = take (boxWidth*boxHeight*4) $ concat $ repeat [0x00,0x00,0xff,0xff]
+
+        image = BS.pack $ reds ++ blues ++ greens
+
+    (el, _) <- elAttr' "canvas" (canvasAttrs canvasWidth canvasHeight) $ return ()
+    let canvasElement = castToHTMLCanvasElement(_element_raw el)
+        elementContext =  getContext canvasElement ("2d" :: [Char])
+
+    renderingContext <- fmap CanvasRenderingContext2D $ elementContext
+    imageData <- liftIO $ BS.useAsCStringLen image $ newImageData imageWidth imageHeight 
+    fillStyle <-  liftIO $ (fmap (Just . CanvasStyle) $ toJSVal $ toJSString ("grey" :: [Char]))
+
+    setFillStyle renderingContext fillStyle
+    fillRect renderingContext 0.0 0.0 (fromIntegral canvasWidth) (fromIntegral canvasHeight)
+    putImageData renderingContext (Just $ ImageData imageData) 100 20
 
     return ()
